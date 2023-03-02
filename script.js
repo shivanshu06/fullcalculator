e2690067b649480dc31ecef5274ba392e79d1ecb
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2023-03-02 20:02:56 +0530

    code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('sivanshu');
+const buttons=document.getElementsByClassName('button')
+const inputField=document.getElementsByClassName('display')
+
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click', (event)=>{
+        let m=event.target.innerHTML
+    })
+}
+
